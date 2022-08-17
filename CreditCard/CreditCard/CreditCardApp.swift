ba2337f997808f3eb8e013205aedac37df42cbf1
Эван Крошкин <>
2022-08-17 13:28:02 +0300

    Initial project setup

---
+++ CreditCard/CreditCard/CreditCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardApp.swift
+//  CreditCard
+//
+//  Created by Эван Крошкин on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
